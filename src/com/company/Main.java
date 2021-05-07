a07906e023a8bc947b1a4408f92b8fe2fb83f2c7
Antony Alex <antonyalexcm@gmail.com>
2021-05-08 01:10:48 +0530

    This is a Java code commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // This is  practice code please feel free to ignore
+        int result = 10 + 12;
+        String answer = "The answer is " + result;
+        System.out.println(answer);
+    };
+}
